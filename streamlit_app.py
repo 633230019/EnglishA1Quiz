e28292f3ebd44a63312d8f01622cfba2a6939a1c
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-16 21:29:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,10 +26,8 @@ q_type = st.selectbox(
     'ระบุประเภทแบบทดสอบ',
     ('ทดสอบความรู้คำศัพท์ทั่วไป','ทดสอบความรู้ไวยากรณ์'), key="q_type")
 
-st.markdown("----", unsafe_allow_html=True)
 columns = st.columns((2, 1, 2))
 button_pressed = columns[1].button('สร้างแบบทดสอบ')
-st.markdown("----", unsafe_allow_html=True)
 
 if button_pressed:
     st.session_state.Quiz = generate_quiz(Num_quiz, Num_choice, df_Sent, df_Word)
