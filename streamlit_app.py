a077ca12222b047cb594f178b1dae2fc322f774d
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-16 21:23:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,12 +13,14 @@ df_Word=pd.read_csv("./data/oxford_a1.csv")
 
 st.header("สร้างแบบทดสอบภาษาอังกฤษระดับป.6")
 
-    
-Num_quiz = st.number_input('ระบุจำนวนข้อแบบทดสอบที่ต้องการสร้าง', 
-                        min_value=1, max_value=100, value=20, key="Num_quiz")
+col1, col2 = st.columns(2)
 
-Num_choice = st.number_input('ระบุจำนวนข้อแบบทดสอบที่ต้องการสร้าง', 
-                        min_value=2, max_value=5, value=4, key="Num_choice")
+with col1:
+    Num_quiz = st.number_input('ระบุจำนวนข้อแบบทดสอบที่ต้องการสร้าง', 
+                            min_value=1, max_value=100, value=20, key="Num_quiz")
+with col2:
+    Num_choice = st.number_input('ระบุจำนวนข้อแบบทดสอบที่ต้องการสร้าง', 
+                            min_value=2, max_value=5, value=4, key="Num_choice")
     
 q_type = st.selectbox(
     'ระบุประเภทแบบทดสอบ',
@@ -30,22 +32,9 @@ if create_quiz:
     switch_page("quiz_generate")
 
 with ui.card(key="card1"):
-    ui.element("span", children=["Number of Quiz"], className="text-gray-400 text-sm font-medium m-1", key="label1")
-    num_quiz = st.number_input('Enter the number of quiz questions', min_value=1, max_value=100, value=20, key="Num_quiz")
+    ui.element("span", children=["Email"], className="text-gray-400 text-sm font-medium m-1", key="label1")
+    ui.element("input", key="email_input", placeholder="Your email")
 
-    ui.element("span", children=["Number of Choices"], className="text-gray-400 text-sm font-medium m-1", key="label2")
-    num_choice = st.number_input('Enter the number of choices', min_value=2, max_value=5, value=4, key="Num_choice")
-
-    q_type = st.selectbox(
-        'Select the quiz type',
-        ('General Vocabulary', 'Grammar'), key="q_type")
-
-    create_quiz_button = ui.element("button", text="Create Quiz", key="button", className="m-1")
-    create_quiz = st.button("Create Quiz")
-
-# Handle button click
-if create_quiz:
-    st.session_state.Quiz = generate_quiz(num_quiz, num_choice, df_Sent, df_Word)
-    st.write("Quiz generated successfully!")
-    st.session_state.show_quiz = True
-    
+    ui.element("span", children=["User Name"], className="text-gray-400 text-sm font-medium m-1", key="label2")
+    ui.element("input", key="username_input", placeholder="Create a User Name")
+    ui.element("button", text="Submit", key="button", className="m-1")
