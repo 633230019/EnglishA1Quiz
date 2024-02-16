ffa7a198a201a631c204fb1e827b1cd3a965537c
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-16 18:58:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,19 +11,19 @@ df_Word=pd.read_csv("./data/oxford_a1.csv")
 
 st.header("สร้างแบบทดสอบภาษาอังกฤษระดับป.6")
 
-st.number_input('ระบุจำนวนข้อแบบทดสอบที่ต้องการสร้าง',
-min_value=1, max_value=100, value=int, key="Num_Quiz")
+Num_quiz = st.number_input('ระบุจำนวนข้อแบบทดสอบที่ต้องการสร้าง',
+min_value=1, max_value=100, value=int, key="Num_quiz")
 
     
-st.session_state.Num_choice = st.selectbox(
+Num_choice = st.session_state.Num_choice = st.selectbox(
     'ระบุจำนวนตัวเลือกของแต่ละข้อแบบทดสอบ',
     ('2','3','4','5'), key="Num_choice")
     
-st.session_state.Quiz_Type = st.selectbox(
+q_type = st.session_state.Quiz_Type = st.selectbox(
     'ระบุประเภทแบบทดสอบ',
     ('ทดสอบความรู้คำศัพท์ทั่วไป'), key="q_type")
 
 create_quiz = st.button("สร้างแบบทดสอบ")
 if create_quiz:
-    st.session_state.Quiz = generate_quiz(st.session_state.Num_Quiz, st.session_state.Num_choice, df_Sent, df_Word)
+    st.session_state.Quiz = generate_quiz(st.session_state.Num_quiz, st.session_state.Num_choice, df_Sent, df_Word)
     switch_page("1-quiz_generate")
