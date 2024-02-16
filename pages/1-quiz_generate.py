8712b39b2748b824a954032524401185d1c7c56b
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-16 22:22:08 +0700

    Update 1-quiz_generate.py

--- pages/1-quiz_generate.py
+++ pages/1-quiz_generate.py
@@ -6,11 +6,14 @@ from spacy.tokens import Token
 from utils.func import generate_quiz
 from streamlit_extras.switch_page_button import switch_page
 
-
-st.header("สร้างแบบทดสอบเรียบร้อย")
-st.markdown("""---""")
-
-quiz = st.button("เปิด")
-if quiz:
-    switch_page("quiz_page")
+with st.container(border=True):
+    st.header("สร้างแบบทดสอบเรียบร้อย")
+    st.markdown("""---""")
+    st.subheader("ข้อมูลแบบทดสอบ")
+    st.caption(f"จำนวนข้อ: {st.session_state.Num_quiz} ข้อ")
+    st.caption(f"จำนวนตัวเลือก: {st.session_state.Num_choice} ตัวเลือก")
+    st.caption(f"ประเภท: {st.session_state.q_type}")
+    quiz = st.button("เปิด")
+    if quiz:
+        switch_page("quiz_page")
 
