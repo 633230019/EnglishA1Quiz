f3ab1c83ec93b439d07e5cd5620a63db2ed53eac
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-16 21:56:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,22 @@ import pandas as pd
 from spacy.tokens import Token
 from utils.func import generate_quiz
 from streamlit_extras.switch_page_button import switch_page
-import streamlit_shadcn_ui as ui
+from streamlit_extras.stylable_container import stylable_container
 
 df_Sent=pd.read_csv("./data/filtered_sentences_dataset_a1_20k.csv")
 df_Word=pd.read_csv("./data/oxford_a1.csv")
 
-with st.container(border=True):
+with stylable_container(
+    key="home_container",
+    css_styles="""
+        {
+            background-color: rgba(255, 255, 255, 1)
+            border: 1px solid rgba(49, 51, 63, 0.2);
+            border-radius: 0.5rem;
+            padding: calc(1em - 1px)
+        }
+        """,
+):
     st.header("สร้างแบบทดสอบภาษาอังกฤษระดับชั้นประถมศึกษาปีที่ 6")
     st.markdown("#")
     col1, col2 = st.columns(2)
