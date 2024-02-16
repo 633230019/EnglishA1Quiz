9acc1dbfb0e7f1c3deba1bdc7edb3f3712545f99
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-16 23:01:14 +0700

    Update 2-quiz_page.py

--- pages/2-quiz_page.py
+++ pages/2-quiz_page.py
@@ -24,57 +24,54 @@ Num_quiz = len(st.session_state.Quiz)
 Num_choice = len(st.session_state.Quiz[0]["choices"])
 q_type = 'ประเภท: ความรู้คำศัพท์ทั่วไป'
 
-colbutton, colquiz = st.columns([0.3,0.7],gap="medium")
-with colbutton:
-    new_q = st.button("สร้างแบบทดสอบใหม่")
-    if new_q:
-        switch_page("streamlit_app")
 
-with colquiz:
-    with st.container(border=True):
-        tab1, tab2, = st.tabs(["แบบทดสอบ", "เฉลย"])
-        with tab1:
-            st.subheader("แบบทดสอบภาษาอังกฤษระดับชั้นประถมศึกษาปีที่ 6")
-            col1, col2 = st.columns([0.8, 0.2])
-            with col1:
-                st.markdown(f"**{q_type}**")
-            with col2:
-                st.markdown(f"**จำนวน {Num_quiz} ข้อ**")
-            st.write('***')   
-            st.markdown("**คำชี้แจง**: เลือกคำตอบที่ถูกต้องที่สุดเพียงคำตอบเดียวลงในกระดาษคำตอบ")
+with st.container(border=True):
+    tab1, tab2, = st.tabs(["แบบทดสอบ", "เฉลย"])
+    with tab1:
+        st.subheader("แบบทดสอบภาษาอังกฤษระดับชั้นประถมศึกษาปีที่ 6")
+        col1, col2 = st.columns([0.8, 0.2])
+        with col1:
+            st.markdown(f"**{q_type}**")
+        with col2:
+            st.markdown(f"**จำนวน {Num_quiz} ข้อ**")
+        st.write('***')   
+        st.markdown("**คำชี้แจง**: เลือกคำตอบที่ถูกต้องที่สุดเพียงคำตอบเดียวลงในกระดาษคำตอบ")
+        
+        quiz_list = st.session_state.Quiz
+        for i, q in enumerate(quiz_list, 1):
+            question = q["question"]
+            choices = q["choices"]
             
-            quiz_list = st.session_state.Quiz
-            for i, q in enumerate(quiz_list, 1):
-                question = q["question"]
-                choices = q["choices"]
-                
-                # Write the question number and the question text
-                st.markdown(f"{i}.&nbsp;{question}")
+            # Write the question number and the question text
+            st.markdown(f"{i}.&nbsp;{question}")
 
-                # Iterate over the choices
-                for j, c in enumerate(choices):
-                    # Convert the index j to an alphabetic character
-                    choice_order = chr(ord('A') + j)
-                    # Write the choice number and the choice text
-                    st.markdown(f"&nbsp;&nbsp;&nbsp;&nbsp;{choice_order}.&nbsp;{c}")
+            # Iterate over the choices
+            for j, c in enumerate(choices):
+                # Convert the index j to an alphabetic character
+                choice_order = chr(ord('A') + j)
+                # Write the choice number and the choice text
+                st.markdown(f"&nbsp;&nbsp;&nbsp;&nbsp;{choice_order}.&nbsp;{c}")
+        
+    with tab2:
+        st.subheader("เฉลยแบบทดสอบ")
+        col1, col2 = st.columns([0.8, 0.2])
+        with col1:
+            st.markdown(f"**{q_type}**")
+        with col2:
+            st.markdown(f"**จำนวน {Num_quiz} ข้อ**")
+        st.write('***')   
+        quiz_list = st.session_state.Quiz
+        for i, q in enumerate(quiz_list, 1):
+            question = q["question"]
+            choices = q["choices"]
+            correct_answer = q["correct_answer"]
             
-        with tab2:
-            st.subheader("เฉลยแบบทดสอบ")
-            col1, col2 = st.columns([0.8, 0.2])
-            with col1:
-                st.markdown(f"**{q_type}**")
-            with col2:
-                st.markdown(f"**จำนวน {Num_quiz} ข้อ**")
-            st.write('***')   
-            quiz_list = st.session_state.Quiz
-            for i, q in enumerate(quiz_list, 1):
-                question = q["question"]
-                choices = q["choices"]
-                correct_answer = q["correct_answer"]
-                
-                for j, c in enumerate(choices):
-                    choice_order = chr(ord('A') + j)
-                    if c == correct_answer:
-                        correct_anwser = f"**{choice_order}. {c}**"
-                st.markdown(f"{i}.&nbsp;{question}&nbsp;&nbsp;&nbsp;&nbsp;{correct_anwser}")
+            for j, c in enumerate(choices):
+                choice_order = chr(ord('A') + j)
+                if c == correct_answer:
+                    correct_anwser = f"**{choice_order}. {c}**"
+            st.markdown(f"{i}.&nbsp;{question}&nbsp;&nbsp;&nbsp;&nbsp;{correct_anwser}")
 
+new_q = st.button("สร้างแบบทดสอบใหม่")
+if new_q:
+    switch_page("streamlit_app")
