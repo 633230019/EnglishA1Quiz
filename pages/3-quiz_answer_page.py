8e33c02c1cffe52d3a42dcfd5e95d3fe9d1dce21
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-16 19:10:00 +0700

    Update 3-quiz_answer_page.py

--- pages/3-quiz_answer_page.py
+++ pages/3-quiz_answer_page.py
@@ -19,13 +19,13 @@ for i, q in enumerate(quiz_list):
     correct_answer = q["correct_answer"]
     
     # Write the question number and the question text
-    st.write(f"Question {i + 1}: {question}")
+    st.write(f"{i + 1}. {question}")
 
     # Iterate over the choices
     for j, c in enumerate(choices):
         choice_order = chr(ord('A') + j)
         if c == correct_answer:
             st.write(f"**{choice_order}. {c}**")
-        st.write(f"{choice_order}. {c}")
+        else: st.write(f"{choice_order}. {c}")
     # Add an empty line between questions
     st.write()
