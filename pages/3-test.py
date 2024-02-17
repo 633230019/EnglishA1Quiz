7c520e4f47542d7534b2b55345dc1ded42bdcce0
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-17 19:15:24 +0700

    Update 3-test.py

--- pages/3-test.py
+++ pages/3-test.py
@@ -2,6 +2,13 @@ from base64 import b64encode
 from fpdf import FPDF
 import streamlit as st
 
+
+Num_quiz = len(st.session_state.Quiz)
+Num_choice = len(st.session_state.Quiz[0]["choices"])
+q_type = 'ประเภท: ความรู้คำศัพท์ทั่วไป'
+quiz_list = st.session_state.Quiz
+
+
 st.title("Demo of fpdf2 usage with streamlit")
 
 @st.cache
@@ -9,7 +16,26 @@ def gen_pdf():
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Helvetica", size=24)
-    pdf.cell(text="hello world")
+    pdf.cell("แบบทดสอบภาษาอังกฤษระดับชั้นประถมศึกษาปีที่ 6", align='C')
+    pdf.Ln()
+    pdf.cell(q_type, align='L')
+    pdf.cell(Num_quiz, align='R')
+    pdf.Ln()
+    pdf.cell("คำชี้แจง: เลือกคำตอบที่ถูกต้องที่สุดเพียงคำตอบเดียวลงในกระดาษคำตอบ", align='L')
+    for i, q in enumerate(quiz_list, 1):
+        question = q["question"]
+        choices = q["choices"]
+        
+        # Write the question number and the question text
+        pdf.cell(text=f"{i}. {question}", align='L')
+        pdf.Ln()
+
+        for j, c in enumerate(choices):
+            # Convert the index j to an alphabetic character
+            choice_order = chr(ord('A') + j)
+            # Write the choice number and the choice text
+            pdf.cell(f"{:4}{choice_order}. {c}", align='L')
+            pdf.Ln()
     return bytes(pdf.output())
 
 # Embed PDF to display it:
