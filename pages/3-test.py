aefb4ad95df464fb22b82da5df5397e87c436490
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-17 20:43:52 +0700

    Update 3-test.py

--- pages/3-test.py
+++ pages/3-test.py
@@ -44,10 +44,10 @@ def gen_pdf():
             choice_order = chr(ord('A') + j)
             # Write the choice number and the choice text
             quiz += f"    {choice_order}. {c}"
-            if j < (len(choices)-1):
-                quiz += "\n"
-            else:
+            if (j+1) == len(choices):
                 continue
+            else:
+                quiz += "\n"
         pdf.multi_cell(0, 7, quiz)
 
     # for i, q in enumerate(quiz_list, 1):
