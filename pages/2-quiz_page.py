bbad753ff63ace3b38113757775b834d42fe606a
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-20 09:32:01 +0700

    Update 2-quiz_page.py

--- pages/2-quiz_page.py
+++ pages/2-quiz_page.py
@@ -82,16 +82,16 @@ try:
                 st.markdown(f"{i}.&nbsp;{question}&nbsp;&nbsp;&nbsp;&nbsp;{correct_anwser}")
 
 
-    # ปุ่มดาวน์โหลด pdf ใช้ฟังชั่น gen_pdf() ในไฟล์ func.py 
+    # ปุ่มดาวน์โหลด pdf ใช้ฟังชั่น gen_pdf() ในไฟล์ func.py
+    if st.download_button:
+        with st.spinner('กำลังสร้างไฟล์แบบทดสอบ...'):
+            st.session_state.pdf = gen_pdf()              
     st.download_button(
         label="ดาวน์โหลดแบบทดสอบ",
         data=st.session_state.pdf,
         file_name=f"grade6englishquiz.pdf",
         mime="application/pdf",
     )
-    if st.download_button:
-        with st.spinner('กำลังสร้างไฟล์แบบทดสอบ...'):
-            st.session_state.pdf = gen_pdf() 
 
     # ปุ่มย้อนกลับไปหน้าแรก
     new_q = st.button("สร้างแบบทดสอบใหม่")
