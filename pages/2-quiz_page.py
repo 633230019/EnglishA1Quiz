f7d894cec8f13738dfc1ff19d6b0e93408c368d4
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-20 08:39:14 +0700

    Update 2-quiz_page.py

--- pages/2-quiz_page.py
+++ pages/2-quiz_page.py
@@ -83,12 +83,13 @@ try:
 
 
     # ปุ่มดาวน์โหลด pdf ใช้ฟังชั่น gen_pdf() ในไฟล์ func.py
-    st.download_button(
-        label="ดาวน์โหลดแบบทดสอบ",
-        data=gen_pdf(),
-        file_name=f"grade6englishquiz.pdf",
-        mime="application/pdf",
-    )
+    with st.spinner('กำลังสร้างไฟล์แบบทดสอบ...'):    
+        st.download_button(
+            label="ดาวน์โหลดแบบทดสอบ",
+            data=gen_pdf(),
+            file_name=f"grade6englishquiz.pdf",
+            mime="application/pdf",
+        )
 
     # ปุ่มย้อนกลับไปหน้าแรก
     new_q = st.button("สร้างแบบทดสอบใหม่")
