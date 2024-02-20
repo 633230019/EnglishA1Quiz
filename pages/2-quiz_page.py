de97d8b30084643d0b001ce746aeae46bdaafbb3
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-20 10:47:58 +0700

    Update 2-quiz_page.py

--- pages/2-quiz_page.py
+++ pages/2-quiz_page.py
@@ -106,7 +106,7 @@ try:
     
     if st.session_state.clicked_download:
         st.download_button(
-            label="ดาวน์โหลดไฟล์",
+            label="ดาวน์โหลด",
             data=st.session_state.pdf,
             file_name=f"grade6englishquiz.pdf",
             mime="application/pdf",
