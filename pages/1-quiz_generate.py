fc74097aae5e8940afe9906b1efe0dda3b6cbe7b
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-20 09:51:29 +0700

    Update 1-quiz_generate.py

--- pages/1-quiz_generate.py
+++ pages/1-quiz_generate.py
@@ -27,7 +27,7 @@ try:
         st.markdown(f"จำนวนข้อ: {Num_quiz} ข้อ")
         st.markdown(f"จำนวนตัวเลือก: {Num_choice} ตัวเลือก")
         st.markdown(f"ประเภท: {q_type}")
-        quiz = st.button("เปิด", disable=st.session_state.disabled)
+        quiz = st.button("เปิด", disable=False)
         if quiz:
             switch_page("quiz_page")
         st.session_state.disabled = False
