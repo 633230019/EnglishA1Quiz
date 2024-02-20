d71222d3679de339e41cb3fe9f020035d703894c
633230019 <144575516+633230019@users.noreply.github.com>
2024-02-20 10:10:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ with st.container(border=True):
         q_type_code = "2"
 
     # ปุ่มสร้างแบบทดสอบ
-    button_pressed = button('สร้างแบบทดสอบ', use_container_width=True)
+    button_pressed = st.button('สร้างแบบทดสอบ', use_container_width=True)
 
 
 
