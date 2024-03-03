08763a48a018abf0cf0687b1c469c3a80fb8c6eb
633230019 <144575516+633230019@users.noreply.github.com>
2024-03-04 06:05:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,14 +43,17 @@ try:
     #navigation_bar()
     st.markdown("<h2 style='text-align: center;'>ระบบสร้างแบบทดสอบภาษาอังกฤษระดับชั้นประถมศึกษาปีที่ 6</h2>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center;'>สร้างแบบทดสอบภาษาอังกฤษประเภทเติมคำในช่องว่างแบบอัตโนมัติ</p>", unsafe_allow_html=True)
+    st.markdown("#")
+    st.markdown("#")
+    st.markdown("#")
 
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(2)
     with col1:
         start = st.button("เกี่ยวกับระบบ", use_container_width=True)
         if start:
             switch_page("about_page")
             st.container.empty()
-    with col3:
+    with col2:
         start = st.button("เริ่มสร้างแบบทดสอบ", use_container_width=True)
         if start:
             switch_page("quiz_generate")
