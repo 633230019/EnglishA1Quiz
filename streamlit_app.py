617f49074c03e5e8b25f8fcbc2b396fa7571654b
633230019 <144575516+633230019@users.noreply.github.com>
2024-03-04 06:07:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,28 +39,25 @@ def navigation_bar():
 
 
     
-try:
-    #navigation_bar()
-    st.markdown("<h2 style='text-align: center;'>ระบบสร้างแบบทดสอบภาษาอังกฤษระดับชั้นประถมศึกษาปีที่ 6</h2>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center;'>สร้างแบบทดสอบภาษาอังกฤษประเภทเติมคำในช่องว่างแบบอัตโนมัติ</p>", unsafe_allow_html=True)
-    st.markdown("#")
-    st.markdown("#")
-    st.markdown("#")
-    st.markdown("#")
-    st.markdown("#")
-    st.markdown("#")
 
-    col1, col2 = st.columns(2)
-    with col1:
-        start = st.button("เกี่ยวกับระบบ", use_container_width=True)
-        if start:
-            switch_page("about_page")
-            st.container.empty()
-    with col2:
-        start = st.button("เริ่มสร้างแบบทดสอบ", use_container_width=True)
-        if start:
-            switch_page("quiz_generate")
-            st.container.empty()
+navigation_bar()
+st.markdown("<h2 style='text-align: center;'>ระบบสร้างแบบทดสอบภาษาอังกฤษระดับชั้นประถมศึกษาปีที่ 6</h2>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center;'>สร้างแบบทดสอบภาษาอังกฤษประเภทเติมคำในช่องว่างแบบอัตโนมัติ</p>", unsafe_allow_html=True)
+st.markdown("#")
+st.markdown("#")
+st.markdown("#")
+st.markdown("#")
+st.markdown("#")
+st.markdown("#")
 
-except Exception:
-    st.markdown(''':red[เกิดข้อผิดพลาด กรุณาสร้างแบบทดสอบใหม่อีกครั้ง]''')
+col1, col2 = st.columns(2)
+with col1:
+    start = st.button("เกี่ยวกับระบบ", use_container_width=True)
+    if start:
+        switch_page("about_page")
+        st.container.empty()
+with col2:
+    start = st.button("เริ่มสร้างแบบทดสอบ", use_container_width=True)
+    if start:
+        switch_page("quiz_generate")
+        st.container.empty()
