834019bdfeeb2573835426ceaf846908e3cfef3e
633230019 <144575516+633230019@users.noreply.github.com>
2024-05-10 17:41:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ st.markdown(
 
 with st.container(border=True):
     st.markdown("<h3 style='text-align: center;'>ระบบสร้างแบบทดสอบภาษาอังกฤษระดับชั้นประถมศึกษาปีที่ 6 แบบอัตโนมัติ</h2>", unsafe_allow_html=True)
-    st.markdown("<h5 style='text-align: center;'>สร้างแบบทดสอบภาษาอังกฤษประเภทเติมคำในช่องว่างแบบอัตโนมัติ</h5>", unsafe_allow_html=True)
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center;'>สร้างแบบทดสอบภาษาอังกฤษประเภทเติมคำในช่องว่างแบบอัตโนมัติ</h6>", unsafe_allow_html=True)
     # st.markdown("สร้างแบบทดสอบภาษาอังกฤษประเภทเติมคำในช่องว่างแบบอัตโนมัติ")
     st.markdown("#")
     start = st.button("เริ่มสร้างแบบทดสอบ", use_container_width=True)
