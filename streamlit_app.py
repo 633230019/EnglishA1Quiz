f25f7f9ffa1d3434b01245e98db170bf3eb700f1
633230019 <144575516+633230019@users.noreply.github.com>
2024-05-10 17:17:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,21 +35,21 @@ with st.container(border=True):
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    with st.container(height=500,border=False):
+    with st.container(height=300,border=True):
         st.markdown("<h4 style='text-align: center;'>1. สร้างแบบทดสอบ</h4>", unsafe_allow_html=True)
         st.markdown("#")
         st.markdown("ใส่จำนวนข้อสอบที่ต้องการสร้าง และข้อมูลอื่น ๆ เพื่อสร้างแบบทดสอบ")
 
 
 with col2:
-    with st.container(height=500,border=False):
+    with st.container(height=300,border=True):
         st.markdown("<h4 style='text-align: center;'>2. รับแบบทดสอบและเฉลยแบบทดสอบ</h4>", unsafe_allow_html=True)
         st.markdown("#")
         st.markdown("รับแบบทดสอบและเฉลยแบบทดสอบตามข้อมูลที่ได้ระบุไว้")
 
 
 with col3:
-    with st.container(height=500,border=False):
+    with st.container(height=300,border=True):
         st.markdown("<h4 style='text-align: center;'>3. ดาวน์โหลด</h4>", unsafe_allow_html=True)
         st.markdown("#")
         st.markdown("บันทึกแบบทดสอบและเฉลยแบบทดสอบเป็นไฟล์ PDF")
